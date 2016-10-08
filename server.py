bcee33d0e9fc776354279255fbef91d6ac760fbd
Munsanje Mweene <mmweene15@gmail.com>
2016-10-08 20:52:46 +0200

    Fixed directory issue

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 import requests
 import tempfile
 import matplotlib
@@ -119,4 +120,6 @@ def graph_class():
 
 
 if __name__ == '__main__':
+    if not os.path.exists('static/temp'):
+        os.makedirs('static/temp')
     app.run(debug=True, host='0.0.0.0')
